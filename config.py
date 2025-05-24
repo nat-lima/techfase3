b3ca9bc2c951ba6597a3cd682a41bab50f48a51c
Natalia Lima <61159855+nat-lima@users.noreply.github.com>
2025-05-24 14:56:00 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,57 @@
+<<<<<<< HEAD
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    CACHE_TYPE = 'simple'
+    SWAGGER = {
+        'title': 'Dados Abertos PRF',
+        'uiversion': 3,
+
+        "paths": {
+                    "/scrape/dadosabertosprf": {
+                    "get": {
+                        "tags": [
+                        "Produção"
+                        ],
+                        "summary": "Retrieve data from 2025 e 2024 from https://www.gov.br/prf/pt-br/acesso-a-informacao/dados-abertos/dados-abertos-acidentes (Producao). ",
+                        "responses": {
+                        "200": {
+                            "description": "Successfull",
+                            "content": {
+                            "application/json": {
+                                "schema": {
+                                "$ref": "#/scrape/"
+                                }
+                            }
+                            }
+                        }
+                        }
+                        }
+                    },
+                    "/auth/login": {
+                    "post": {
+                        "tags": [
+                        "Login"
+                        ],
+    "requestBody": { "required": True, "content": { "application/json": { "schema": { "$ref": "#/components/schemas/LoginRequest" } } } } ,        
+    "summary": "HTTP Request for login",
+                        "responses": {
+                        "200": {
+                            "description": "Successful",
+                            "content": {
+                            "application/json": {
+                                "schema": {
+                                "$ref": "#/auth/"
+                                }
+                            }
+                            }
+                        }
+                        }
+                        }
+                    }
+            }
+    }
+
+=======
 class Config:
     SECRET_KEY = 'your_secret_key'
     CACHE_TYPE = 'simple'
@@ -147,3 +201,4 @@ class Config:
             }
     }
 
+>>>>>>> 5d8c14d8b292a4a718c24f2f8749b4d5dc2161f8
